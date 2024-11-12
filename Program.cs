8b397e0b50e24fd2d6f90576a2ffabde78ba788c
conno <conno@CONSTRAPCUM>
2024-11-12 16:21:40 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using _2._2._1practice;
+
+namespace _2._2._1practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {           
+            Console.WriteLine("Select the shape (Circle or Square):");
+            string shapeType = Console.ReadLine().ToLower();
+            Shape shape;
+            if (shapeType == "circle")
+            {
+                shape = new Circle();
+                Console.WriteLine("Enter the radius of the circle:");
+                ((Circle)shape).radius = double.Parse(Console.ReadLine());  
+            }
+            else if (shapeType == "square")
+            {
+                shape = new Square();
+                Console.WriteLine("Enter the side length of the square:");
+                ((Square)shape).SideLength = double.Parse(Console.ReadLine());
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Invalid shape type.");
+                return;
+            }
+
+            Console.WriteLine("Enter the ID of the shape:");
+            shape.Id = int.Parse(Console.ReadLine());//.Parse used to covert the string to an integer 
+            Console.WriteLine("Enter the name of the shape:");
+            shape.Name = Console.ReadLine();//User inputs shape name 
+            Console.WriteLine("Enter the color of the shape:");
+            shape.Color = Console.ReadLine();//User inputs shape color 
+            Console.WriteLine($"The area of the {shape.Name} is: {shape.CalculateArea()}");
+        }
+            
+    }
+}
---
+++ models/Circle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._2._1practice
+{
+    public class Circle : Shape
+    {
+        public double radius { get; set; }
+
+        public override double CalculateArea()
+        {            
+            return Math.PI * radius * radius;            
+        }
+    }
+}
